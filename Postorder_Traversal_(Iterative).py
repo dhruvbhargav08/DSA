ea30e45d5de2f4e7edcae036377525379b5c9104
Dhruv Bhargav <97904433+dhruvbhargav08@users.noreply.github.com>
2023-01-23 23:40:53 +0530

    Add files via upload

---
+++ Postorder_Traversal_(Iterative).py
@@ -0,0 +1,131 @@
+# #This is from Geeks for Geeks.Question link at last
+# Given a binary tree. Find the postorder traversal of the tree without using recursion.
+#User function Template for python3
+
+'''
+# Node Class:
+class Node:
+    def init(self,val):
+        self.data = val
+        self.left = None
+        self.right = None
+'''
+class Solution:
+    # Return a list containing the inorder traversal of the given tree
+    def peek(self,stack):
+        if len(stack) > 0:
+            return stack[-1]
+        return None
+    def postOrder(self,root):
+        # code here
+        listt=[]
+        stack=[]
+        while True:
+            while root:
+                if root.right is not None:
+                    stack.append(root.right)
+                stack.append(root)
+                root=root.left
+            root=stack.pop()
+            if (root.right!=None) and (self.peek(stack)==root.right):
+                stack.pop()
+                stack.append(root)
+                root=root.right
+            else:
+                listt.append(root.data)
+                root=None
+            if len(stack)<=0:
+                break
+        return listt
+            
+            
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+#Initial Template for Python 3
+
+from collections import deque
+# Tree Node
+class Node:
+    def __init__(self, val):
+        self.right = None
+        self.data = val
+        self.left = None
+
+    
+# Function to Build Tree   
+def buildTree(s):
+    #Corner Case
+    if(len(s)==0 or s[0]=="N"):           
+        return None
+        
+    # Creating list of strings from input 
+    # string after spliting by space
+    ip=list(map(str,s.split()))
+    
+    # Create the root of the tree
+    root=Node(int(ip[0]))                     
+    size=0
+    q=deque()
+    
+    # Push the root to the queue
+    q.append(root)                            
+    size=size+1 
+    
+    # Starting from the second element
+    i=1                                       
+    while(size>0 and i<len(ip)):
+        # Get and remove the front of the queue
+        currNode=q[0]
+        q.popleft()
+        size=size-1
+        
+        # Get the current node's value from the string
+        currVal=ip[i]
+        
+        # If the left child is not null
+        if(currVal!="N"):
+            
+            # Create the left child for the current node
+            currNode.left=Node(int(currVal))
+            
+            # Push it to the queue
+            q.append(currNode.left)
+            size=size+1
+        # For the right child
+        i=i+1
+        if(i>=len(ip)):
+            break
+        currVal=ip[i]
+        
+        # If the right child is not null
+        if(currVal!="N"):
+            
+            # Create the right child for the current node
+            currNode.right=Node(int(currVal))
+            
+            # Push it to the queue
+            q.append(currNode.right)
+            size=size+1
+        i=i+1
+    return root
+    
+    
+if __name__=="__main__":
+    t=int(input())
+    for _ in range(0,t):
+        s=input()
+        root=buildTree(s)
+        ob = Solution()
+        res = ob.postOrder(root)
+        for i in range (len(res)):
+            print (res[i], end = " ")
+        print()
+        
+        
+
+
+# } Driver Code Ends
+# Question link
+# https://practice.geeksforgeeks.org/problems/postorder-traversal-iterative/1?page=3&difficulty[]=1&status[]=solved&sortBy=submissions
