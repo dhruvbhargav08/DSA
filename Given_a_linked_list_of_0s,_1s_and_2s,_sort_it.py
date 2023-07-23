819c0eaeb1ed842e57dc7be893b4b9003de2f8d4
Dhruv Bhargav <97904433+dhruvbhargav08@users.noreply.github.com>
2023-07-23 22:43:29 +0530

    23-07-2023

---
+++ Given_a_linked_list_of_0s,_1s_and_2s,_sort_it.py
@@ -0,0 +1,106 @@
+#This is from Geeks for Geeks.Question link at last
+# Given a linked list of N nodes where nodes can contain values 0s, 1s, and 2s only. 
+# The task is to segregate 0s, 1s, and 2s linked list such that all zeros segregate to head side, 2s at the end of the linked list, and 1s in the mid of 0s and 2s.
+#User function Template for python3
+'''
+	Your task is to segregate the list of 
+	0s,1s and 2s.
+	
+	Function Arguments: head of the original list.
+	Return Type: head of the new list formed.
+
+	{
+		# Node Class
+		class Node:
+		    def __init__(self, data):   # data -> value stored in node
+		        self.data = data
+		        self.next = None
+	}
+
+'''
+class Solution:
+    #Function to sort a linked list of 0s, 1s and 2s.
+    def segregate(self, head):
+        #code here
+        listt=[0]*3
+        temp=head
+        while temp:
+            listt[temp.data%3]+=1
+            temp=temp.next
+        i=0
+        newhead=Node(0)
+        prev=newhead
+        while i<3:
+            if listt[i]>0:
+                temp=Node(i)
+                prev.next=temp
+                prev=temp
+                listt[i]-=1
+            if listt[i]==0:
+                i+=1
+        return newhead.next
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+#Contributed by : Nagendra Jha
+
+import atexit
+import io
+import sys
+
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+@atexit.register
+
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+# Node Class
+class Node:
+    def __init__(self, data):   # data -> value stored in node
+        self.data = data
+        self.next = None
+
+# Linked List Class
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    # creates a new node with given value and appends it at the end of the linked list
+    def append(self, new_value):
+        new_node = Node(new_value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+            return
+        self.tail.next = new_node
+        self.tail = new_node
+        
+# prints the elements of linked list starting with head
+def printList(head):
+    if head is None:
+        print(' ')
+        return
+    curr_node = head
+    while curr_node:
+        print(curr_node.data,end=" ")
+        curr_node=curr_node.next
+    print()
+
+if __name__ == '__main__':
+    t=int(input())
+    for cases in range(t):
+        n = int(input())
+        a = LinkedList() # create a new linked list 'a'.
+        nodes_a = list(map(int, input().strip().split()))
+        for x in nodes_a:
+            a.append(x)  # add to the end of the list
+        printList(Solution().segregate(a.head))
+# } Driver Code Ends
+# Question link
+# https://practice.geeksforgeeks.org/problems/given-a-linked-list-of-0s-1s-and-2s-sort-it/1
